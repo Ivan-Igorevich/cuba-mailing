b4a31bdad642bdf6a0c905683f5273aabe56f24a
Ivan I. Ovchinnikov <ovchinnikov_ii@risde.ru>
2019-12-13 13:02:39 +0300

    user "Sysyem" empty marker user

---
+++ modules/core/db/init/postgres/30.create-db.sql
@@ -0,0 +1,4 @@
+--
+
+insert into SEC_USER (ID, VERSION, CREATE_TS, CREATED_BY, UPDATE_TS, UPDATED_BY, DELETE_TS, DELETED_BY, LOGIN, LOGIN_LC, PASSWORD, PASSWORD_ENCRYPTION, NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, POSITION_, EMAIL, LANGUAGE_, TIME_ZONE, TIME_ZONE_AUTO, ACTIVE, CHANGE_PASSWORD_AT_LOGON, GROUP_ID, IP_MASK) values
+('a48ca93e-ac48-740c-4ef1-4b46833bf826', 0, null, null, null, null, null, null, 'System', 'system', '$2a$10$Dj6C9ZDAy8RmyGr0SIbo6uT8GMR2ZjDjD8RDQ.vnYmfLDRh/ampTe', 'bcrypt', 'System ', 'System', null, null, null, null, 'en', null, null, true, false, '0fa2b1a5-1d68-4d69-9fbd-dff348347f93', null);
